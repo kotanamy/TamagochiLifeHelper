3d5a41ec76405d7609a544b1526e047986198541
kotanamy <donat0009@outlook.com>
2022-03-27 11:03:35 +0300

    Initial Commit

---
+++ TamagochiLifeHelper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TamagochiLifeHelper
+//
+//  Created by Ярослав Котов on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
